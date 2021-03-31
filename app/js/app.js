8f570065af8330b706880c7160835a37a16b571a
Maksim Koshman <maksimkoshman090@gmail.com>
2021-03-31 16:11:03 +0300

    Сделал шапку и список новостей

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,78 @@
-$(document).ready(function () {});
+document.addEventListener("DOMContentLoaded", function () {
+  // Поиск родителя
+  function findAncestor(el, cls) {
+    while ((el = el.parentElement) && !el.classList.contains(cls));
+    return el;
+  }
+
+  // Селект
+  function closeSelects() {
+    const selects = document.querySelectorAll(".exchange__select");
+    selects.forEach((select) => {
+      select.classList.remove("exchange__select--active");
+    });
+  }
+
+  const selectsHead = document.querySelectorAll(".exchange__select__head");
+  if (selectsHead) {
+    selectsHead.forEach((selectHead) => {
+      selectHead.addEventListener("click", () => {
+        const currSelect = selectHead.parentNode;
+
+        if (currSelect.classList.contains("exchange__select--active")) {
+          currSelect.classList.remove("exchange__select--active");
+        } else {
+          closeSelects();
+          currSelect.classList.add("exchange__select--active");
+        }
+      });
+    });
+  }
+
+  if (selectsHead) {
+    document.onclick = function (evt) {
+      if (
+        evt.target.className != "exchange__select" &&
+        !findAncestor(evt.target, "exchange__select")
+      ) {
+        closeSelects();
+      }
+    };
+  }
+
+  const selectItems = document.querySelectorAll(".exchange__select__item");
+  if (selectItems) {
+    selectItems.forEach((selectItem) => {
+      selectItem.addEventListener("click", () => {
+        const currSelect = findAncestor(selectItem, "exchange__select");
+        const currSelectTitle = currSelect.querySelector(
+          ".exchange__select__title"
+        );
+        currSelectTitle.innerHTML = selectItem.innerHTML;
+        currSelect.classList.remove("exchange__select--active");
+      });
+    });
+  }
+
+  const openSelect = document.querySelector("#openSelect");
+  if (openSelect) {
+    openSelect.addEventListener("click", (evt) => {
+      evt.preventDefault();
+      openSelect.parentNode.classList.toggle("exchange__inner__choose--active");
+    });
+  }
+
+  // News List Slider
+  if (document.querySelector("#newsListSlider")) {
+    new Glider(document.querySelector("#newsListSlider"), {
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      draggable: true,
+      dots: false,
+      arrows: {
+        prev: ".news__list__arrow--prev",
+        next: ".news__list__arrow--next",
+      },
+    });
+  }
+});
